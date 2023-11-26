7fd2a17b8b8979a162170a15f23ccf0380f9bd0a
George Nicolaou <orionas.elite@gmail.com>
2023-11-26 16:09:19 +0000

    done

--- gn-custom-order-status.php
+++ gn-custom-order-status.php
@@ -70,5 +70,83 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 	'gn-custom-order-status'
 );
 
+function gncy_register_custom_order_delivered_status() {
+	register_post_status( 'wc-delivered', array(
+		'label'                     => _x( 'Delivered', 'Order status', 'woocommerce' ),
+		'public'                    => true,
+		'exclude_from_search'       => false,
+		'show_in_admin_all_list'    => true,
+		'show_in_admin_status_list' => true,
+		'label_count'               => _n_noop( 'Delivered <span class="count">(%s)</span>', 'Delivered <span class="count">(%s)</span>', 'woocommerce' )
+	) );
+}
+add_action( 'init', 'gncy_register_custom_order_delivered_status' );
+
+function gncy_add_custom_order_delivered_status_to_order_statuses( $order_statuses ) {
+	$new_order_statuses = array();
+	// add new order status after processing
+	foreach ( $order_statuses as $key => $status ) {
+		$new_order_statuses[ $key ] = $status;
+		if ( 'wc-processing' === $key ) {
+			$new_order_statuses['wc-delivered'] = __( 'Delivered', 'woocommerce' );
+		}
+	}
+	return $new_order_statuses;
+}
+
+add_filter( 'wc_order_statuses', 'gncy_add_custom_order_delivered_status_to_order_statuses' );
+
+//add custom order status to admin order list bulk actions
+function gncy_add_custom_order_status_bulk_actions( $bulk_actions ) {
+	$bulk_actions['mark_delivered'] = __( 'Mark Delivered', 'woocommerce' );
+	return $bulk_actions;
+}
+add_filter( 'bulk_actions-edit-shop_order', 'gncy_add_custom_order_status_bulk_actions' );
+
+//add custom order status to dropdown in order edit screen
+function gncy_add_custom_order_status_to_dropdown( $order_statuses ) {
+	$new_order_statuses = array();
+	// add new order status after processing
+	foreach ( $order_statuses as $key => $status ) {
+		$new_order_statuses[ $key ] = $status;
+		if ( 'wc-processing' === $key ) {
+			$new_order_statuses['wc-delivered'] = __( 'Delivered', 'woocommerce' );
+		}
+	}
+	return $new_order_statuses;
+}
+add_filter( 'wc_order_statuses', 'gncy_add_custom_order_status_to_dropdown' );
+
+
+
+  
+function gncy_change_woocommerce_strings_emails( $translated, $untranslated, $domain ) {
+   if ( 'woocommerce' === $domain ) {   
+      $translated = str_ireplace( 'We have finished processing your order.', 'Your order has been delivered', $untranslated ); // EDIT
+   }
+   return $translated;
+}
+
+
+
+
+//send email notification when order status is changed to delivered with custom email subject and content using a custom email templated based on the completed email notification
+function gncy_send_custom_email_notification( $order_id, $old_status, $new_status ) {
+	if ( $new_status == 'delivered' ) {
+		$order = wc_get_order( $order_id );
+		$wc_emails = WC()->mailer()->get_emails(); // Get all WC_emails objects instances
+		$wc_emails['WC_Email_Customer_Completed_Order']->heading = 'Your order is delivered'; // Changing the email heading
+		$wc_emails['WC_Email_Customer_Completed_Order']->subject = 'Your order is delivered'; // Changing the email subject
+		$wc_emails['WC_Email_Customer_Completed_Order']->settings['heading'] = 'Your order is delivered';
+		$wc_emails['WC_Email_Customer_Completed_Order']->settings['subject'] = 'Your order is delivered';
+		add_filter( 'gettext', 'gncy_change_woocommerce_strings_emails', 9999, 3 );
+		$wc_emails['WC_Email_Customer_Completed_Order']->trigger( $order_id ); // Sending the email
+
+		
+}
+
+}
+add_action( 'woocommerce_order_status_changed', 'gncy_send_custom_email_notification', 10, 3 );
+
 //Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('main');
